7476f2700b3c81951e0bb5b12057c1364756680c
Omar-Slik <92442894+Omar-Slik@users.noreply.github.com>
2021-11-05 16:11:41 +0100

    Problem_refresh

--- ConsoleFileExplorer/ConsoleExplorer.cs
+++ ConsoleFileExplorer/ConsoleExplorer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,24 +10,95 @@ namespace ConsoleFileExplorer
 {
     class ConsoleExplorer
     {
+        public ViewState _viewState = ViewState.List;
+        private FolderView folderView = new(Path.GetDirectoryName
+            (Assembly.GetExecutingAssembly().Location));
         public void Run()
-        {                       
+        {
             while (true)
             {
-                string[] dirs = Directory.GetFileSystemEntries(".");
-                foreach (string dirName in dirs)
+                Console.Clear();
+                switch (_viewState)
                 {
-                    if (File.Exists(dirName))
-                    {
-                        Console.WriteLine($"- {Path.GetFileName(dirName)}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"# {Path.GetFileName(dirName)}");
-                    }
+                    case ViewState.List:
+                        ShowList();
+                        break;
+                    case ViewState.FileView:
+                        ShowFile();
+                        break;
+                    case ViewState.CreateFile:
+                        CreateFile();
+                        break;
+                    //case ViewState.DeleteFile:
+                    //    DeleteFile();
+                    //    break;
                 }
-                Console.ReadKey();
             }
+        }
+        private void ShowList()
+        {
+            folderView.PrintList();
+            InputKey();
+        }
+        private void ShowFile()
+        {
+            folderView.FileContent();
+            _viewState = ViewState.List;
+        }
+        private void InputKey()
+        {
+            ConsoleKeyInfo input = Console.ReadKey();
+            switch (input.Key)
+            {
+                case ConsoleKey.Backspace:
+                    folderView = new FolderView(Directory.GetParent
+                    (Directory.GetCurrentDirectory()).FullName);
+                    break;
+                case ConsoleKey.Enter:
+                    folderView.ChangeDirectory();
+                    break;
+                case ConsoleKey.Spacebar:
+                    _viewState = ViewState.FileView;
+                    break;
+                case ConsoleKey.UpArrow:
+                    folderView.Up();
+                    break;
+                case ConsoleKey.DownArrow:
+                    folderView.Down();
+                    break;
+                case ConsoleKey.C:
+                    _viewState = ViewState.CreateFile;
+                    break;
+                case ConsoleKey.D:
+                    DeleteFile();
+                    //_viewState = ViewState.DeleteFile;
+                    break;
+            }
+        }
+        private void CreateFile()
+        {
+            Console.WriteLine("You are creating a new file.\n" +
+                "Please enter the name of the file");
+            string NewFileName = Console.ReadLine() + ".txt";
+            Console.WriteLine("-------------------------------\n" +
+                "Write a text in your file." +
+                "\nWhen done, press enter");
+            string input = "My text";
+            FileStream fs = File.OpenWrite(NewFileName);
+            using (StreamWriter sw = new(fs))
+            {
+                while (input != "")
+                {
+                    input = Console.ReadLine();
+                    sw.WriteLine(input);
+                }
+            }            
+            _viewState = ViewState.List;
+        }
+        private void DeleteFile()
+        {
+            folderView.DeleteFile();
+            _viewState = ViewState.List;
         }
     }
 }
--- ConsoleFileExplorer/FolderView.cs
+++ ConsoleFileExplorer/FolderView.cs
@@ -1,34 +1,94 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-
+using System.Text;
 
 namespace ConsoleFileExplorer
 {
     class FolderView
     {
-        private string _path;
-
+        private int status = 0;
+        public string Path { get; private set; }
+        private string[] dirs;
         public FolderView(string path)
         {
-            _path = path;
+            Path = path;
+            CountDirectoryItems();
+            Directory.SetCurrentDirectory(Path);
         }
-
-        public List<string> GetFolderContent()
+        public void PrintList()
         {
-            List<string> result = new List<string>();
-            string[] dirs = Directory.GetFileSystemEntries(".");
+            //CountDirectoryItems();
             foreach (string dirName in dirs)
             {
-                if (File.Exists(dirName))
+                if (dirs[status] == dirName)
                 {
-                    result.Add($"- {Path.GetFileName(dirName)}");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Path = dirName;
                 }
+                if (File.Exists(dirName))
+                    Console.WriteLine($"- {System.IO.Path.GetFileName(dirName)}");
                 else
+                    Console.WriteLine($"# {System.IO.Path.GetFileName(dirName)}");
+                Console.ResetColor();
+            }
+            Console.WriteLine("-------------------------------");
+        }
+        public void Up()
+        {
+            if (status > 0)
+                status -= 1;
+        }
+        public void Down()
+        {
+            if (status < dirs.Length - 1)
+                status += 1;
+        }
+        public void DeleteFile()
+        {
+            if (File.Exists(Path))
+            {
+                Console.Clear();
+                Console.WriteLine($"Are you sure you want to delete this file\"{Path}\"?\n press y/n");
+                ConsoleKeyInfo deleteCheck = Console.ReadKey(true);
+                if (deleteCheck.Key == ConsoleKey.Y)
                 {
-                    result.Add($"# {Path.GetFileName(dirName)}");
+                    File.Delete(Path);
+                    Console.WriteLine("File has been deleted successfully!");
+                    Console.ReadLine();
+                }
+                else if (deleteCheck.Key == ConsoleKey.N)
+                {
+                    Console.WriteLine("File will not be deleted!");
+                    Console.ReadLine();
                 }
             }
-            return result;
+        }
+        public void ChangeDirectory()
+        {
+            if (Directory.Exists(Path))
+            {
+                Path = dirs[status];
+                status = 0;
+                CountDirectoryItems();
+                Directory.SetCurrentDirectory(Path);
+            }
+        }
+        private void CountDirectoryItems()
+        {
+            dirs = Directory.GetFileSystemEntries(Path);
+        }
+        public void FileContent()
+        {
+            if (File.Exists(Path))
+            {
+                FileStream fs = File.OpenRead(Path);
+                using StreamReader sr = new StreamReader(fs);
+                Console.WriteLine(sr.ReadToEnd());
+                Console.WriteLine("------------------------------- \n" +
+                    "Press any key to return to the list");
+                Console.ReadKey();
+            }
         }
     }
 }
